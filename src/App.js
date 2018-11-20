5e283454c075688e1ddee4c501bc20570ed17c52
Vegitarius <gordonv15@gmail.com>
2018-11-19 19:16:27 -0800

    added ability to update location, based on button pushes

--- src/App.js
+++ src/App.js
@@ -5,14 +5,71 @@ import Month from './Month';
 import config from './config';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      location: '',
+      city: '',
+      state: '',
+      zipCode: '',
+      county: '',
+      country: '',
+      latitude: '',
+      longitude: ''
+    }
+  }
+
+  buttonPusher() {
+    console.log("lat", this.state.latitude, "long", this.state.longitude)
+    if (this.state.latitude) {
+      this.coder();
+      console.log(this.state.location)
+    }
+    if (this.state.city) {
+      console.log(this.state.location);
+    }
+  }
+
+  finder() {
+    let success = (position) => {
+      this.setState({
+        latitude: String(position.coords.latitude),
+        longitude: String(position.coords.longitude)
+      })
+    }
+    let error = () => {
+      console.log("No Location")
+    }
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+  coder = () => {
+      fetch("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + 
+              this.state.latitude + 
+              "," +
+              this.state.longitude + 
+              "&key=" + 
+              config.locationKey)
+        .then(response => response.json())  
+        .then(data => {this.setState({
+          location: data,
+          city: data.results[4].address_components[1].long_name,
+          state: data.results[4].address_components[3].short_name,
+          zipCode: data.results[4].address_components[0],
+          county: data.results[4].address_components[2],
+          country: data.results[4].address_components[4]
+      })})
+    } 
+
   render() {
     let date = new Date();
-   
     return (
       <div className="App">
         <a href="https://darksky.net/poweredby/">Powered by Dark Sky</a>
-        <Current />
+        <Current city={this.state.city} state={this.state.state} />
         <Month date={date} />
+        <button onClick={ () => this.buttonPusher() }>Geocode</button>
+        <button onClick={ () =>  this.finder() }>Find Me</button>
       </div>
     );
   }
--- src/Current.js
+++ src/Current.js
@@ -1,35 +1,51 @@
 import React from 'react';
 import './Current.css';
 
-const Current = () => {
-  return (
-    <div id="current">
-      <div id="location">
-        Puyallup, WA
-      </div>
-      <div id="date">
-        Today
-      </div>
-      <div id="weather-img"  className="col2">
-        <img src={require("./assets/cloudy-sym.jpg")} alt="cloudy-symbol" />
-      </div>
-      <div id="col1">
+export default class Current extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      place: "New York, New York"
+    }
+  }
 
-        <div id="current-temperature">
-          52&deg;
+  updateArea() {
+    if (this.props.city && this.props.state) {
+      this.setState({
+        place: this.props.city + ", " + this.props.state
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div id="current">
+        <div id="location">
+          {this.state.place}
         </div>
-        <div id="current-weather">
-          LIGHT DRIZZLE
+        <div id="date">
+          Today
         </div>
-        <div id="current-feels-like">
-          Feels Like 52&deg;
+        <div id="weather-img"  className="col2">
+          <img src={require("./assets/cloudy-sym.jpg")} alt="cloudy-symbol" />
         </div>
-        <div id="today-high-low">
-          Hi 60&deg; Low 49&deg;
+        <div id="col1">
+
+          <div id="current-temperature">
+            52&deg;
+          </div>
+          <div id="current-weather">
+            LIGHT DRIZZLE
+          </div>
+          <div id="current-feels-like">
+            Feels Like 52&deg;
+          </div>
+          <div id="today-high-low">
+            Hi 60&deg; Low 49&deg;
+          </div>
         </div>
+        <button onClick={() => {this.updateArea()}}>Update</button>
       </div>
-    </div>
-  )
+    )
+  }
 }
-
-export default Current;
