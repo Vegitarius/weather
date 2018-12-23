198f7ca167980e631472996ec55f028a95bdcbda
Vegitarius <gordonv15@gmail.com>
2018-12-23 06:10:00 -0800

    working on sizing on mobile

--- src/components/Period/Period.js
+++ src/components/Period/Period.js
@@ -6,10 +6,10 @@ import '../../style.css';
 const Period = (props) => {
   return (
     <ul id="period" className="flex-c text-c size-med">
-      <li className='per-link'><Link className='per-nav' to='/week'>Weekly</Link></li>
-      <li className='per-link'><Link className='per-nav' to='/hour'>Hourly</Link></li>
-      <li className='per-link'><Link className='per-nav' to='/current'>Current</Link></li>
-      <li className='per-link'><Link className='per-nav' to='/minute'>Minutely</Link></li>
+      <li className='per-link'><Link className='per-nav' to='/week'><button>Weekly</button></Link></li>
+      <li className='per-link'><Link className='per-nav' to='/hour'><button>Hourly</button></Link></li>
+      <li className='per-link'><Link className='per-nav' to='/current'><button>Current</button></Link></li>
+      <li className='per-link'><Link className='per-nav' to='/minute'><button>Minutely</button></Link></li>
     </ul>
   )
 }
