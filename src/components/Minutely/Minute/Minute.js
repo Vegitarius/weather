2edf68219627df3c193feb46c7faf86c02f282f8
Vegitarius <gordonv15@gmail.com>
2018-12-26 03:45:25 -0800

    start of minutes component worked out, shows weather precip or not

--- src/components/Minutely/Minute/Minute.js
+++ src/components/Minutely/Minute/Minute.js
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import './Minute.css';
 
 class Minute extends Component {
+  getTime(hour, minute) {
+    let hours = hour, suffix;
+    switch(true) {
+      case (hour === 0):
+        hours = hour + 12;
+        suffix = 'am'
+        break;
+      case (hour === 12):
+        suffix = 'pm';
+        break;
+      case (hour > 12):
+        hours = hour - 12;
+        suffix = 'pm';
+        break;
+      case (hour < 12):
+        suffix = 'am';
+        break;
+      default:
+        break;
+    }
+    return hours + ':' + minute + ' ' + suffix;
+  }
   render() {
+    const { weather } = this.props;
+    const date = new Date(weather.time * 1000);
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
     return (
-      <div>
-
+      <div className='minute'>
+        <p>{this.getTime(hour, minutes)}</p>
+        <p>{(weather.precipProbability * 100).toFixed()}% chance of {weather.precipType || 'none'}</p>
       </div>
     )
   }
--- src/components/Minutely/Minutely.js
+++ src/components/Minutely/Minutely.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Minute from './Minute/Minute';
 import './Minutely.css';
 
+const mapStateToProps = state => ({
+  weather: state.handleWeather.weather,
+  location: state.translateLocation.location 
+})
+
 class Minutely extends Component {
   render() {
+    const { weather, location } = this.props;
+    let minutes = [], summary;
+    if (weather) {
+      console.log(weather);
+      summary = weather.minutely.summary;
+      for (let i = 0; i < weather.minutely.data.length; i++) {
+        let minuteWeather = weather.minutely.data[i];
+        minutes.push(
+          <Minute 
+            key = {i}
+            weather = {minuteWeather}
+          />
+        )
+      }
+    }
     return (
-      <div>
-        <Minute />
+      <div id="minutely-weather">
+        <span><h2>{location}</h2>
+        <p>{summary}</p></span>
+        <div id='minutes-container'>{minutes}</div>
       </div>
     )
   }
 }
 
-export default Minutely;
+export default connect(mapStateToProps, null)(Minutely);
